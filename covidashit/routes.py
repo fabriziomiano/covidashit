78c0713dcd78025ad7e56bc9034c3c3c101a924c
Fabrizio Miano <fabriziomiano@gmail.com>
2020-03-21 03:39:28 +0100

    v0.0.2: Fix datetime format

--- covidashit/routes.py
+++ covidashit/routes.py
@@ -5,13 +5,13 @@ from covidashit import app, get_data
 
 @app.route('/')
 @app.route('/index')
-def index(chart_id='chart_ID', chart_type='bar'):
+def index(chart_id='chart_ID', chart_type='column'):
     dates, series1, series2, series3, series4 = get_data()
     chart = {"renderTo": chart_id, "type": chart_type}
     series = [series1, series2, series3, series4]
     title = {"text": 'COVID-19 Italian trend'}
     x_axis = {"categories": dates}
-    y_axis = {"title": {"text": '# of people'}}
+    y_axis = {"title": {"text": '# of people'}, "type": "logarithmic"}
     return render_template(
         'index.html',
         chartID=chart_id,
--- covidashit/static/js/main.js
+++ covidashit/static/js/main.js
@@ -1,9 +1,37 @@
-$(document).ready(function() {
-	$(chart_id).highcharts({
-		chart: chart,
-		title: title,
-		xAxis: xAxis,
-		yAxis: yAxis,
-		series: series
-	});
+let url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json";
+$(document).ready(function () {
+    $(chart_id).highcharts({
+        exporting: {
+            buttons: {
+                customButton: {
+                    text: 'Linear',
+                    onclick: function () {
+                        this.yAxis[0].update({
+                            type: 'linear'
+                        });
+                    }
+                },
+                customButton2: {
+                    text: 'Logarithmic',
+                    onclick: function () {
+                        this.yAxis[0].update({
+                            type: 'logarithmic'
+                        });
+                    }
+                },
+            }
+        },
+        chart: chart,
+        title: title,
+        xAxis: xAxis,
+        yAxis: yAxis,
+        series: series,
+        subtitle: {
+            text: ('Source: <a href="' + url + '">Protezione Civile</a>')
+        },
+        credits: {
+            href: "fabriziomiano.github.io",
+            text: "by Fabrizio Miano"
+        }
+    });
 });
