3a7dc283dcd6e6b45f19edfac74412b8d32fe379
Fabrizio Miano <fabriziomiano@gmail.com>
2020-03-23 00:35:38 +0100

    add subtitle

--- covidashit/static/js/main.js
+++ covidashit/static/js/main.js
@@ -1,9 +1,13 @@
-$(document).ready(function() {
-	$(chart_id).highcharts({
-		chart: chart,
-		title: title,
-		xAxis: xAxis,
-		yAxis: yAxis,
-		series: series
-	});
+let url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json";
+$(document).ready(function () {
+    $(chart_id).highcharts({
+        chart: chart,
+        title: title,
+        xAxis: xAxis,
+        yAxis: yAxis,
+        series: series,
+        subtitle: {
+            text: ('Source: <a href='+ url +'> Protezione Civile </a>')
+        },
+    });
 });
