dbfd1ed5ed09591d068459a228cad2e16bd8f2f6
Fabrizio Miano <fabriziomiano@gmail.com>
2020-03-23 20:50:57 +0100

    avoid JS caching, use title and timestamp

--- covidashit/routes.py
+++ covidashit/routes.py
@@ -1,6 +1,7 @@
+import time
 from flask import render_template
-
 from covidashit import app, get_data
+PAGE_TITLE = "COVID-19 Italian trend"
 
 
 @app.route('/')
@@ -9,14 +10,17 @@ def index(chart_id='chart_ID', chart_type='column'):
     dates, series1, series2, series3, series4 = get_data()
     chart = {"renderTo": chart_id, "type": chart_type}
     series = [series1, series2, series3, series4]
-    title = {"text": 'COVID-19 Italian trend'}
+    title = {"text": 'COVID-19 Italian trend', "align": "left"}
     x_axis = {"categories": dates}
     y_axis = {"title": {"text": '# of people'}, "type": "logarithmic"}
     return render_template(
         'index.html',
+        pagetitle=PAGE_TITLE,
         chartID=chart_id,
         chart=chart,
         series=series,
         title=title,
         xAxis=x_axis,
-        yAxis=y_axis)
+        yAxis=y_axis,
+        ts=str(time.time())
+    )
