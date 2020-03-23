a86561fe5cb3a3ac83c9aa642a85ad00ca7b9ad8
Fabrizio Miano <fabriziomiano@gmail.com>
2020-03-23 19:16:37 +0100

    add linear/log toggle in exporting button

--- covidashit/static/js/main.js
+++ covidashit/static/js/main.js
@@ -1,6 +1,26 @@
 let url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json";
 $(document).ready(function () {
     $(chart_id).highcharts({
+        exporting: {
+            buttons: {
+                customButton: {
+                    text: 'Linear',
+                    onclick: function () {
+                        this.yAxis[0].update({
+                            type: 'linear'
+                        });
+                    }
+                },
+                customButton2: {
+                    text: 'Logarithmic',
+                    onclick: function () {
+                        this.yAxis[0].update({
+                            type: 'logarithmic'
+                        });
+                    }
+                },
+            }
+        },
         chart: chart,
         title: title,
         xAxis: xAxis,
