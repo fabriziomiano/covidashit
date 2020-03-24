90428249e8dbb3a879b0b8f1145aea8bad83521b
Fabrizio Miano <fabriziomiano@gmail.com>
2020-03-24 01:13:46 +0100

    v0.2.0: Use all the data provided by the civil protection

--- covidashit/__init__.py
+++ covidashit/__init__.py
@@ -25,21 +25,42 @@ def get_data():
     dates = []
     intensive_care = []
     hospitalized_w_symptoms = []
-    total_cases = []
+    tot_dead = []
+    tot_hospitalized = []
+    isolated = []
+    tot_curr_pos = []
+    new_curr_pos = []
+    tot_swabs = []
     healed = []
+    total_cases = []
     for d in data:
         date_dt = dt.datetime.strptime(d["data"], DATE_FMT)
         date_str = date_dt.strftime("%d %b")
         dates.append(date_str)
         intensive_care.append(d["terapia_intensiva"])
         hospitalized_w_symptoms.append(d["ricoverati_con_sintomi"])
-        total_cases.append(d["deceduti"])
+        tot_dead.append(d["deceduti"])
         healed.append(d["dimessi_guariti"])
-    series1 = {"name": "Intensive Care", "data": intensive_care}
+        tot_hospitalized.append(d["totale_ospedalizzati"])
+        isolated.append(d["isolamento_domiciliare"])
+        tot_curr_pos.append(d["totale_attualmente_positivi"])
+        new_curr_pos.append(d["nuovi_attualmente_positivi"])
+        tot_swabs.append(d["tamponi"])
+        total_cases.append(d["totale_casi"])
+    series1 = {"name": "Intensive Care", "data": intensive_care, "visible": "true"}
     series2 = {"name": "Hospitalized with symptoms", "data": hospitalized_w_symptoms}
-    series3 = {"name": "Dead", "data": total_cases}
+    series3 = {"name": "Dead", "data": tot_dead}
     series4 = {"name": "Healed", "data": healed}
-    return dates, series1, series2, series3, series4
+    series5 = {"name": "Hospitalized", "data": tot_hospitalized}
+    series6 = {"name": "Isolation", "data": isolated}
+    series7 = {"name": "Currently positive", "data": tot_curr_pos}
+    series8 = {"name": "New currently positive", "data": new_curr_pos}
+    series9 = {"name": "Swabs", "data": tot_swabs}
+    series10 = {"name": "Tot cases", "data": total_cases}
+    return (
+        dates, series1, series2, series3, series4, series5,
+        series6, series7, series8, series9, series10
+    )
 
 
 app = Flask(__name__)
--- covidashit/routes.py
+++ covidashit/routes.py
@@ -7,9 +7,13 @@ PAGE_TITLE = "COVID-19 Italian trend"
 @app.route('/')
 @app.route('/index')
 def index(chart_id='chart_ID', chart_type='column'):
-    dates, series1, series2, series3, series4 = get_data()
+    (dates, series1, series2, series3, series4, series5,
+     series6, series7, series8, series9, series10) = get_data()
     chart = {"renderTo": chart_id, "type": chart_type}
-    series = [series1, series2, series3, series4]
+    series = [
+        series1, series2, series3, series4, series5,
+        series6, series7, series8, series9, series10
+    ]
     title = {"text": 'COVID-19 Italian trend', "align": "left"}
     x_axis = {"categories": dates}
     y_axis = {"title": {"text": '# of people'}, "type": "logarithmic"}
