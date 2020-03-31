a2787f53594066073abbe62fafd3959ad31c3187
Fabrizio Miano <fabriziomiano@gmail.com>
2020-03-31 16:14:36 +0200

    change ITEN_MAP structure and MAIN_TYPES
    Signed-off-by: Fabrizio Miano <fabriziomiano@gmail.com>

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import datetime as dt
 
-
 URL_NATIONAL_DATA = (
     "https://raw.githubusercontent.com"
     "/pcm-dpc/COVID-19/master/dati-json/"
@@ -17,21 +16,54 @@ URL_PROVINCIAL_DATA = (
     "dpc-covid19-ita-province.json"
 )
 ITEN_MAP = {
-    "ricoverati_con_sintomi": "Hospitalized with symptoms",
-    "terapia_intensiva": "Intensive Care Unit",
-    "totale_ospedalizzati": "Hospitalized",
-    "isolamento_domiciliare": "Self Isolation",
-    "totale_positivi": "Total positive",
-    "variazione_totale_positivi": "Total positive variation",
-    "nuovi_positivi": "New positive",
-    "dimessi_guariti": "Total Healed",
-    "deceduti": "Total Deaths",
-    "totale_casi": "Total cases",
-    "tamponi": "Total Swabs"
+    "ricoverati_con_sintomi": {
+        "title": "Hospitalized with symptoms",
+        "desc": ""
+    },
+    "terapia_intensiva": {
+        "title": "Intensive Care Unit",
+        "desc": "# of people in ICU"
+    },
+    "totale_ospedalizzati": {
+        "title": "Total Hospitalized",
+        "desc": "Tot # of people hospitalized (today)"
+    },
+    "isolamento_domiciliare": {
+        "title": "Self Isolation",
+        "desc": ""
+    },
+    "totale_positivi": {
+        "title": "Total positive",
+        "desc": ""
+    },
+    "variazione_totale_positivi": {
+        "title": "Total positive variation",
+        "desc": "Tot Positive (Today) - Tot Positive (Yesterday)"
+    },
+    "nuovi_positivi": {
+        "title": "New positive",
+        "desc": "Tot Cases (Today) - Tot cases (Yesterday)"
+    },
+    "dimessi_guariti": {
+        "title": "Total Healed",
+        "desc": ""
+    },
+    "deceduti": {
+        "title": "Total Deaths",
+        "desc": ""
+    },
+    "totale_casi": {
+        "title": "Total cases",
+        "desc": ""
+    },
+    "tamponi": {
+        "title": "Total Swabs",
+        "desc": ""
+    }
 }
 MAIN_TYPES = [
     "nuovi_positivi", "variazione_totale_positivi",
-    "terapia_intensiva", "ricoverati_con_sintomi"
+    "terapia_intensiva", "totale_ospedalizzati"
 ]
 REGION_KEY = "denominazione_regione"
 PROVINCE_KEY = "denominazione_provincia"
