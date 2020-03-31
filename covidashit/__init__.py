01c096d3c7b5faa569a6f1bc785d49c49760d967
Fabrizio Miano <fabriziomiano@gmail.com>
2020-03-31 16:12:51 +0200

    add after request decoration to avoid browser caching
    Signed-off-by: Fabrizio Miano <fabriziomiano@gmail.com>

--- covidashit/__init__.py
+++ covidashit/__init__.py
@@ -2,4 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 from covidashit import routes, views
