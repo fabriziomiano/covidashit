396a6c778daaff9d9ebe5f5bc73d0b567c2f1d37
Fabrizio Miano <fabriziomiano@gmail.com>
2020-04-03 13:59:54 +0200

    modify PCM URL
    use the 'dati-json' directory on the repo, rather than pointing to the raw json

--- covidashit/static/js/main.js
+++ covidashit/static/js/main.js
@@ -1,4 +1,4 @@
-let url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json";
+let url = "https://github.com/pcm-dpc/COVID-19/tree/master/dati-json";
 $(document).ready(function () {
     $(chart_id).highcharts({
         chart: chart,
