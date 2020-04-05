2ff2c67fbf73955a8815901fb03a5090182ebdd5
Fabrizio Miano <fabriziomiano@gmail.com>
2020-04-06 00:02:06 +0200

    decouple national from regional and provincial routes; use "territory" instead

--- covidashit/views.py
+++ covidashit/views.py
@@ -1,9 +1,13 @@
-import time
-import json
 import datetime as dt
+import json
+import time
+
 from flask import render_template
 from flask_babel import gettext
-from config import WEBSITE_TITLE, LOCKDOWN_DAY
+
+from config import (
+    WEBSITE_TITLE, LOCKDOWN_DAY, CHART_TYPE, CHART_ID, REGIONS, PROVINCES
+)
 from covidashit import app
 from covidashit.dataset import init_data, parse_data, init_chart, latest_update
 from covidashit.routes import (
@@ -11,36 +15,55 @@ from covidashit.routes import (
 )
 
 
-@app.route('/')
-@app.route('/regions/<string:region>')
-@app.route('/provinces/<string:province>')
-def provincial(region=None, province=None, chart_id='chart_ID', chart_type='line'):
-    data = json.loads(get_provincial_data().data)
-    data.update(json.loads(get_regional_data().data))
-    data.update(json.loads(get_national_data().data))
+@app.route("/")
+def national():
+    data = json.loads(get_national_data().data)
     init_data()
-    if province is not None:
-        dates, series, trend, regions, provinces = parse_data(data, province=province)
-        chart_title = {"text": province, "align": "left"}
-        updated_at = latest_update(data["provincial"])
-    elif region is not None:
-        dates, series, trend, regions, provinces = parse_data(data, region=region)
-        chart_title = {"text": region, "align": "left"}
-        updated_at = latest_update(data["regional"])
-    else:
-        dates, series, trend, regions, provinces = parse_data(data)
-        chart_title = {"text": gettext("Italy"), "align": "left"}
-        updated_at = latest_update(data["national"])
-    chart, x_axis, y_axis = init_chart(chart_id, chart_type, dates)
+    dates, series, trend = parse_data(data)
+    chart_title = {"text": gettext("Italy"), "align": "left"}
+    updated_at = latest_update(data["national"])
+    chart, x_axis, y_axis = init_chart(CHART_ID, CHART_TYPE, dates)
     return render_template(
-        'dashboard.html',
+        "dashboard.html",
         trend=trend,
-        regions=regions,
-        region=region,
-        provinces=provinces,
-        province=province,
+        regions=REGIONS,
+        provinces=PROVINCES,
         pagetitle=gettext(WEBSITE_TITLE),
-        chartID=chart_id,
+        chartID=CHART_ID,
+        chart=chart,
+        series=series,
+        chart_title=chart_title,
+        xAxis=x_axis,
+        yAxis=y_axis,
+        ts=str(time.time()),
+        lockdown_days=(dt.datetime.today() - LOCKDOWN_DAY).days,
+        latest_update=updated_at
+    )
+
+
+@app.route("/regions/<string:territory>")
+@app.route("/provinces/<string:territory>")
+def provincial(territory):
+    if territory in REGIONS:
+        data = json.loads(get_regional_data().data)
+        updated_at = latest_update(data["regional"])
+    elif territory in PROVINCES:
+        data = json.loads(get_provincial_data().data)
+        updated_at = latest_update(data["provincial"])
+    else:
+        return render_template("404.html", pagetitle=WEBSITE_TITLE)
+    init_data()
+    dates, series, trend = parse_data(data, territory=territory)
+    chart_title = {"text": territory, "align": "left"}
+    chart, x_axis, y_axis = init_chart(CHART_ID, CHART_TYPE, dates)
+    return render_template(
+        "dashboard.html",
+        trend=trend,
+        territory=territory,
+        provinces=PROVINCES,
+        regions=REGIONS,
+        pagatitle=gettext(WEBSITE_TITLE),
+        chartID=CHART_ID,
         chart=chart,
         series=series,
         chart_title=chart_title,
