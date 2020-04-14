778be695c9c3cbdd35666f0d9101874f2d43c16c
Fabrizio Miano <fabriziomiano@gmail.com>
2020-04-15 01:19:18 +0200

    update strings and translations
    Signed-off-by: Fabrizio Miano <fabriziomiano@gmail.com>

--- config.py
+++ config.py
@@ -14,44 +14,56 @@ URL_REGIONAL_DATA = os.path.join(BASE_URL_DATA, REGIONAL_DATA_FILE)
 URL_PROVINCIAL_DATA = os.path.join(BASE_URL_DATA, PROVINCIAL_DATE_FILE)
 ITEN_MAP = {
     "ricoverati_con_sintomi": {
-        "title": gettext("Hospitalized with symptoms"),
-        "desc": gettext("# of People currently in hospital with symptoms"),
+        "title": gettext("Hospitalized With Symptoms"),
+        "desc": gettext(
+            "# of people currently hospitalized with symptoms"
+        ),
         "longdesc": gettext(
-            "Total count of people currently in hospital with symptoms")
+            "Total count of people currently in hospital "
+            "due to coronavirus with symptoms"
+        )
     },
     "terapia_intensiva": {
         "title": gettext("Intensive Care Unit"),
-        "desc": gettext("# of people in ICU"),
+        "desc": gettext("# of people currently in ICU"),
         "longdesc": gettext(
-            "Total count of people currently in ICU  and positive to COVID-19"
+            "Total count of people currently in ICU and positive to COVID-19"
         )
     },
     "totale_ospedalizzati": {
         "title": gettext("Total Hospitalized"),
-        "desc": gettext("# of people hospitalized (today)"),
+        "desc": gettext("# of people currently hospitalized"),
         "longdesc": gettext(
             "Total count of people currently hospitalized. "
             "It takes into account ICU")
     },
     "isolamento_domiciliare": {
         "title": gettext("Self Isolation"),
-        "desc": "",
-        "longdesc": ""
+        "desc": gettext("# of people currently in self isolation"),
+        "longdesc": gettext(
+            "People currently positive but who don't need hospitalization"
+        )
     },
     "totale_positivi": {
-        "title": gettext("Total positive"),
-        "desc": gettext("Hospitalized with symptoms + ICU + Self Isolation"),
+        "title": gettext("Total Positive"),
+        "desc": gettext(
+            "# of people currently "
+            "hospitalized with symptoms + ICU + self isolation"
+        ),
         "longdesc": gettext(
             "People currently positive. "
             "Unlike 'Total Cases' it does not take into account "
             "'healed' and 'deaths'. By the end of the outbreak "
-            "this should tend to zero. In particular, it is "
+            "this should tend to zero. In particular, it is: "
             "total positive = total cases - total healed - total deaths"
         )
     },
     "variazione_totale_positivi": {
-        "title": gettext("Total positive variation"),
-        "desc": gettext("Tot Positive (Today) - Tot Positive (Yesterday)"),
+        "title": gettext("Total Positive Variation"),
+        "desc": gettext(
+            "Variation of # of people currently positive "
+            "with respect to yesterday"
+        ),
         "longdesc": gettext(
             "Variation of the number of people currently positive "
             "with respect to the previous day. It is negative when the number "
@@ -59,31 +71,42 @@ ITEN_MAP = {
         )
     },
     "nuovi_positivi": {
-        "title": gettext("New positive"),
-        "desc": gettext("Tot Cases (Today) - Tot cases (Yesterday)"),
+        "title": gettext("New Positive"),
+        "desc": gettext("Daily count of new positve cases"),
         "longdesc": gettext("Daily count of new positve cases")
     },
     "dimessi_guariti": {
         "title": gettext("Total Healed"),
-        "desc": "",
-        "longdesc": ""
+        "desc": gettext("Cumulative # of people healed"),
+        "longdesc": gettext(
+            "Total number of people healed since the beginning of the outbreak"
+        )
     },
     "deceduti": {
         "title": gettext("Total Deaths"),
-        "desc": "",
-        "longdesc": ""
+        "desc": gettext("Cumulative # of deaths"),
+        "longdesc": gettext(
+            "Total number of people who have died due to coronavirus"
+            " since the beginnin of the outbreak"
+        )
     },
     "totale_casi": {
-        "title": gettext("Total cases"),
-        "desc": gettext("Self isolation + Tot Hospitalized + Tot Healed + Tot Deaths"),
+        "title": gettext("Total Cases"),
+        "desc": gettext(
+            "Total count of the positive tests since the"
+            " beginning of the outbreak"
+        ),
         "longdesc": gettext(
-            "Total count of the positive tests since the beginning of the outbreak"
+            "Total count of the positive tests since the"
+            " beginning of the outbreak"
         )
     },
     "tamponi": {
         "title": gettext("Total Swabs"),
-        "desc": "",
-        "longdesc": ""
+        "desc": gettext("# of swabs performed"),
+        "longdesc": gettext(
+            "Total number of swabs performed since the beginning of the outbreak"
+        )
     }
 }
 CARD_TYPES = [
@@ -144,14 +167,14 @@ CREDITS = {
 SCATTER_XAXIS = {
     "title": {
         "enabled": "true",
-        "text": gettext("# Total cases")
+        "text": gettext("# of Total cases")
     },
     "showLastLabel": "true"
 }
 SCATTER_YAXIS = {
     "type": "logarithmic",
     "title": {
-        "text": gettext("# New positive")
+        "text": gettext("# of New positive")
     }
 }
 SOURCE_SUBTITLE = {
