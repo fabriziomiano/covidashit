2e7c51be37774dd4e3f826b292be35fa2e43f48f
Fabrizio Miano <fabriziomiano@gmail.com>
2020-04-20 18:41:57 +0200

    use const instead of var
    Signed-off-by: Fabrizio Miano <fabriziomiano@gmail.com>

--- covidashit/static/js/scripts.js
+++ covidashit/static/js/scripts.js
@@ -7,7 +7,7 @@
     "use strict";
 
     // Add active state to sidbar nav links
-    var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+    const path = window.location.href; // because the 'href' property of the DOM element is the absolute path
     $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function () {
         if (this.href === path) {
             $(this).addClass("active");
