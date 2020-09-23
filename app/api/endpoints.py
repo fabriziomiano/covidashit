ab86b79ea13c701b1fb3ff5f79c7d768ca2afc23
Fabrizio Miano <fabriziomiano@gmail.com>
2020-09-23 21:58:47 +0200

    refactor; handle no-data possibility

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -3,19 +3,37 @@ from flask import jsonify
 from app.api import api
 from config import COLLECTION_NAME, BARCHART_RACE_QUERY, UPDATE_FMT
 
+import json
+from bson import ObjectId
 
-@api.route("/bcr")
-def get_bcr():
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
+@api.route("/bcr/<string:var_name>")
+def get_bcr(var_name):
     """
     Bar-chart race API. Return a JSON with fields "html" and "ts".
     The former contains the HTML code of the bar-chart race video,
     the latter the timestamp at which the bar-chart race was created on the DB
+    :param: var_name: str
     :return: flask.jsonify()
     """
     from app import mongo
-    bcr_data = next(
-        mongo.db[COLLECTION_NAME].find(BARCHART_RACE_QUERY)
-    )
-    bcr_html = bcr_data["html_str"]
-    bcr_ts = bcr_data["ts"].strftime(UPDATE_FMT)
-    return jsonify(html=bcr_html, ts=bcr_ts)
+    data = {}
+    BARCHART_RACE_QUERY["name"] = var_name
+    try:
+        data = next(
+            mongo.db[COLLECTION_NAME].find(BARCHART_RACE_QUERY)
+        )
+        data["ts"] = data["ts"].strftime(UPDATE_FMT)
+        data["status"] = "ok"
+        data = json.loads(JSONEncoder().encode(data))
+    except StopIteration:
+        data["status"] = "ko"
+        data["error"] = "No data"
+    return jsonify(**data)
