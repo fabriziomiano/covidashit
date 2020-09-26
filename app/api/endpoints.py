ad311e9a73a2c2085d6a1e1c2a550bd1be9a4ad2
Fabrizio Miano <fabriziomiano@gmail.com>
2020-09-26 18:55:34 +0200

    add logging

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, current_app
 
 from app.api import api
 from config import COLLECTION_NAME, BARCHART_RACE_QUERY, UPDATE_FMT
@@ -27,6 +27,7 @@ def get_bcr(var_name):
     data = {}
     BARCHART_RACE_QUERY["name"] = var_name
     try:
+        current_app.logger.info("Getting {} bcr from mongo".format(var_name))
         data = next(
             mongo.db[COLLECTION_NAME].find(BARCHART_RACE_QUERY)
         )
@@ -34,6 +35,8 @@ def get_bcr(var_name):
         data["status"] = "ok"
         data = json.loads(JSONEncoder().encode(data))
     except StopIteration:
+        err_msg = "No {} bcr data in mongo".format(var_name)
+        current_app.logger.error(err_msg)
         data["status"] = "ko"
-        data["error"] = "No data"
+        data["error"] = err_msg
     return jsonify(**data)
