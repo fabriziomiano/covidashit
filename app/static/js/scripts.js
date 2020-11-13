0e8323ae5692327379f08d0b4e060dd0afd39adb
Fabrizio Miano <fabriziomiano@gmail.com>
2020-11-13 19:48:12 +0100

    Add back-to-top button behaviour

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -113,3 +113,22 @@ let provincesUrl = '/provinces/';
     }
 
 })(jQuery);
+
+
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function () {
+    let height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function () {
+    $("#back2Top").click(function (event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
