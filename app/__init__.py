f39290ace1cd285b5c1f65bdd3674a23a1f8c234
Fabrizio Miano <fabriziomiano@gmail.com>
2020-12-08 20:58:28 +0100

    v2.10.0: Add collection creation on server startup if in production

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ Flask application factory
 """
 import os
 
+from dotenv import load_dotenv
 from flask import Flask, request, render_template, send_from_directory
 from flask_babel import Babel
 from flask_pymongo import PyMongo
@@ -23,6 +24,7 @@ def get_locale():
 
 def set_error_handlers(app):
     """Error handler"""
+
     @app.errorhandler(404)
     def page_not_found(e):
         """Page not found"""
@@ -38,6 +40,7 @@ def set_error_handlers(app):
 
 def set_robots_txt_rule(app):
     """Bots rule"""
+
     @app.route("/robots.txt")
     def robots_txt():
         """Serve the robots.txt file"""
@@ -46,6 +49,7 @@ def set_robots_txt_rule(app):
 
 def set_favicon_rule(app):
     """Favicon rule"""
+
     @app.route("/favicon.ico")
     def favicon():
         """Serve the favicon.ico file"""
@@ -56,12 +60,14 @@ def set_favicon_rule(app):
 
 def create_app():
     """Create the flask application"""
+    load_dotenv()
     app = Flask(__name__)
     app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
     app.config["MONGO_URI"] = MONGO_URI
     app.config["SITEMAP_INCLUDE_RULES_WITHOUT_PARAMS"] = True
     translation_dir = os.path.join(app.root_path, TRANSLATION_DIRNAME)
     app.config["BABEL_TRANSLATION_DIRECTORIES"] = translation_dir
+    app.config["TESTING"] = os.getenv("TESTING", default=0)
     mongo.init_app(app)
     babel.init_app(app)
     sitemap.init_app(app)
@@ -86,5 +92,30 @@ def create_app():
 
     from .api import api
     app.register_blueprint(api)
+
+    if not app.config["TESTING"]:
+        """Create and fill the collections if on production server"""
+        from app.db.recovery import (
+            create_national_collection, create_national_series_collection,
+            create_national_trends_collection, create_regional_collection,
+            create_regional_breakdown_collection,
+            create_regional_series_collection,
+            create_regional_trends_collection, create_provincial_collections,
+            create_provincial_breakdown_collection,
+            create_provincial_series_collection,
+            create_provincial_trends_collection
+        )
+        with app.app_context():
+            create_national_collection()
+            create_national_series_collection()
+            create_national_trends_collection()
+            create_regional_collection()
+            create_regional_breakdown_collection()
+            create_regional_series_collection()
+            create_regional_trends_collection()
+            create_provincial_collections()
+            create_provincial_breakdown_collection()
+            create_provincial_series_collection()
+            create_provincial_trends_collection()
 
     return app
