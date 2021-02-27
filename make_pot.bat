ba99c45a69925712084028f36f3483c814dad8dd
Fabrizio Miano <fabriziomiano@gmail.com>
2021-02-27 16:49:15 +0100

    Move scripts

--- make_pot.bat
+++
@@ -1,2 +0,0 @@
-:: Run within the venv
-pybabel extract -F babel.cfg --project=COVIdashIT --version=1.1 --msgid-bugs-address=fabriziomiano@gmail.com --copyright-holder="Fabrizio Miano" -o messages.pot .
