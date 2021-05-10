8d4818850698b8044ffbe49f8a8c589d988d5bcd
Fabrizio Miano <fabriziomiano@gmail.com>
2021-05-10 23:56:08 +0200

    add ajax calls to get vax-chart data

--- app/static/js/vaccines-charts.js
+++ app/static/js/vaccines-charts.js
@@ -1,21 +1,166 @@
-// Administrations per region: defined only for the national vax view
-let adminsPerRegion = {}
+let baseUrl = '/api/vax_charts/'
 if (!REGIONS.includes(area)) {
 
     // Administrations per region
-    $.getJSON('/api/charts/vax-per-data', function (regionChartData) {
-        let adminsPerRegionData = regionChartData.admins_per_region;
-        let expAdminsPerRegionData = regionChartData.expected_admins;
-        let adminsPerRegionCategories = regionChartData.categories;
-        $('#chart-admins-per-region').highcharts({
+    $.ajax(baseUrl + 'region', {
+        dataType: 'json',
+        cache: false,
+        beforeSend: function () {
+            $('#vax-region-loading-spinner').show();
+        },
+        success: function (adminsPerRegion) {
+            let adminsPerRegionData = adminsPerRegion.real;
+            let expAdminsPerRegionData = adminsPerRegion.expected;
+            let adminsPerRegionCategories = adminsPerRegion.categories;
+            $('#vax-region-loading-spinner').hide();
+            $('#chart-admins-per-region').highcharts({
+                chart: {
+                    type: 'bar'
+                },
+                title: {
+                    text: adminsPerRegion.title
+                },
+                plotOptions: {
+                    series: {
+                        dataLabels: {
+                            enabled: true,
+                            formatter: function () {
+                                let pcnt = (this.y / totAdmins) * 100;
+                                return Highcharts.numberFormat(pcnt, '1') + '%';
+                            }
+                        }
+                    }
+                },
+                xAxis: {
+                    lineColor: '#999',
+                    lineWidth: 1,
+                    tickColor: '#666',
+                    tickLength: 3,
+                    categories: adminsPerRegionCategories,
+                },
+                yAxis: {
+                    title: {
+                        enabled: false
+                    },
+                    labels: {
+                        enabled: false
+                    }
+                },
+                series: [{
+                    name: adminsPerRegionData.name,
+                    data: adminsPerRegionData.data
+                }, {
+                    name: expAdminsPerRegionData.name,
+                    data: expAdminsPerRegionData.data,
+                    visible: false
+                }],
+                subtitle: subtitle,
+                credits: credits
+            });
+        }
+    });
+
+    // Administrations trend
+    $.ajax(baseUrl + 'trend', {
+        dataType: 'json',
+        cache: false,
+        beforeSend: function () {
+            $('#vax-timeseries-loading-spinner').show();
+        },
+        success: function (adminsTimeseriesData) {
+            $('#vax-timeseries-loading-spinner').hide();
+            // Vax time series for a given area
+            adminsTimeseriesData.data = adminsTimeseriesData.data.map(function (o, i) {
+                o.visible = i <= 2;
+                return o;
+            })
+            $('#chart-admins-timeseries').highcharts({
+                chart: {
+                    zoomType: 'x',
+                    type: 'spline'
+                },
+                title: {
+                    text: adminsTimeseriesData.title
+                },
+                xAxis: {
+                    type: 'datetime',
+                    categories: adminsTimeseriesData.dates
+                },
+                yAxis: {
+                    title: {
+                        text: adminsTimeseriesData.yAxisTitle
+                    }
+                },
+                tooltip: {
+                    crosshairs: {
+                        width: 1,
+                        color: 'gray',
+                        dashStyle: 'ShortDashDot'
+                    },
+                    shared: true,
+                    split: false,
+                    enabled: true
+                },
+                legend: {
+                    enabled: false
+                },
+
+                plotOptions: {
+                    series: {
+                        label: {
+                            connectorAllowed: false
+                        },
+                        marker: {
+                            enabled: false
+                        }
+                    },
+
+                },
+                series: adminsTimeseriesData.data,
+                credits: credits,
+                subtitle: subtitle
+            });
+
+            let series = $("#chart-admins-timeseries").highcharts().series;
+            let seriesIMap = {}
+            for (let i = 0; i < series.length; i++) {
+                seriesIMap[series[i].name] = i;
+            }
+            $(function () {
+                $('#timeseries-select').change(function (e) {
+                    let selRegions = $(e.target).val();
+                    REGIONS.forEach(function (region) {
+                        selRegions.includes(region) ? series[seriesIMap[region]].show() : series[seriesIMap[region]].hide();
+                    });
+                })
+            });
+        }
+    });
+}
+
+// Administrations per age
+$.ajax(baseUrl + 'age', {
+    dataType: 'json',
+    cache: false,
+    beforeSend: function () {
+        $('#vax-age-loading-spinner').show();
+    },
+    data: {
+        area: area
+    },
+    success: function (adminsPerAge) {
+        $('#vax-age-loading-spinner').hide();
+        $('#chart-admins-per-age').highcharts({
             chart: {
-                type: 'bar'
+                type: 'column'
             },
             title: {
-                text: adminsPerRegionData.title
+                text: adminsPerAge.title + ' | ' + area
             },
             plotOptions: {
                 series: {
+                    shadow: false,
+                    borderWidth: 0,
                     dataLabels: {
                         enabled: true,
                         formatter: function () {
@@ -30,215 +175,125 @@ if (!REGIONS.includes(area)) {
                 lineWidth: 1,
                 tickColor: '#666',
                 tickLength: 3,
-                categories: adminsPerRegionCategories,
+                categories: adminsPerAge.categories,
             },
             yAxis: {
                 title: {
-                    enabled: false
+                    text: adminsPerAge.yAxisTitle
                 },
-                labels: {
-                    enabled: false
-                }
-            },
-            series: [{
-                name: adminsPerRegionData.name,
-                data: adminsPerRegionData.data
-            }, {
-                name: expAdminsPerRegionData.name,
-                data: expAdminsPerRegionData.data,
-                visible: false
-            }],
-            subtitle: subtitle,
-            credits: credits
-        });
-    });
-
-
-    $.getJSON('/api/charts/vax-trend', function (adminsTimeseriesData) {
-        // Vax time series for a given area
-        adminsTimeseriesData.data = adminsTimeseriesData.data.map(function (o, i) {
-            o.visible = i <= 2;
-            return o;
-        })
-        $('#chart-admins-timeseries').highcharts({
-            chart: {
-                zoomType: 'x',
-                type: 'spline'
-            },
-            title: {
-                text: adminsTimeseriesData.title
-            },
-            xAxis: {
-                type: 'datetime',
-                categories: adminsTimeseriesData.dates
-            },
-            yAxis: {
-                title: {
-                    text: adminsTimeseriesData.yAxisTitle
-                }
-            },
-            tooltip: {
-                crosshairs: {
-                    width: 1,
-                    color: 'gray',
-                    dashStyle: 'ShortDashDot'
-                },
-                shared: true,
-                split: false,
-                enabled: true
             },
             legend: {
                 enabled: false
             },
-
-            plotOptions: {
-                series: {
-                    label: {
-                        connectorAllowed: false
-                    },
-                    marker: {
-                        enabled: false
-                    }
-                },
-
-            },
-            series: adminsTimeseriesData.data,
-            credits: credits,
-            subtitle: subtitle
+            series: adminsPerAge.data,
+            subtitle: subtitle,
+            credits: credits
         });
+    }
+});
 
-        let series = $("#chart-admins-timeseries").highcharts().series;
-        let seriesIMap = {}
-        for (let i = 0; i < series.length; i++) {
-            seriesIMap[series[i].name] = i;
-        }
-        $(function () {
-            $('#timeseries-select').change(function (e) {
-                let selRegions = $(e.target).val();
-                REGIONS.forEach(function (region) {
-                    selRegions.includes(region) ? series[seriesIMap[region]].show() : series[seriesIMap[region]].hide();
-                });
-            })
-        });
-    });
-}
-
-// Administrations per Age
-let adminsPerAge = {
-    chart: {
-        type: 'column'
-    },
-    title: {
-        text: adminsPerAgeTitle
-    },
-    plotOptions: {
-        series: {
-            shadow: false,
-            borderWidth: 0,
-            dataLabels: {
-                enabled: true,
-                formatter: function () {
-                    let pcnt = (this.y / totAdmins) * 100;
-                    return Highcharts.numberFormat(pcnt, '1') + '%';
-                }
-            }
-        }
-    },
-    xAxis: {
-        lineColor: '#999',
-        lineWidth: 1,
-        tickColor: '#666',
-        tickLength: 3,
-        categories: ageCategories,
-    },
-    yAxis: {
-        title: {
-            text: adminsPerAgeyAxisTitle
-        },
-    },
-    legend: {
-        enabled: false
-    },
-    series: adminsPerAgeData,
-    subtitle: subtitle,
-    credits: credits
-};
-Highcharts.chart('chart-admins-per-age', adminsPerAge);
 
 // Administrations per category
-let adminsPerCategory = {
-    chart: {
-        plotBackgroundColor: null,
-        plotBorderWidth: null,
-        plotShadow: false,
-        type: 'pie'
+$.ajax(baseUrl + 'category', {
+    area: area,
+    dataType: 'json',
+    cache: false,
+    beforeSend: function () {
+        $('#vax-categories-loading-spinner').show();
     },
-    title: {
-        text: adminsPerCategoryTitle
+    data: {
+        area: area
     },
-    tooltip: {
-        pointFormat: '{series.name}: <b>{point.y}</b>'
-    },
-    accessibility: {
-        point: {
-            valueSuffix: '%'
-        }
-    },
-    plotOptions: {
-        pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: {
-                enabled: true,
-                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                // distance: -1
-            }
-        }
-    },
-    series: [{
-        name: adminsPerCategorySeriesName,
-        data: adminsPerCategoryData
-    }],
-    subtitle: subtitle,
-    credits: credits
-}
-Highcharts.chart('chart-pie-categories', adminsPerCategory);
+    success: function (adminsPerCategory) {
+        $('#vax-categories-loading-spinner').hide();
+        $('#chart-pie-categories').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: adminsPerCategory.title + ' | ' + area
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.y}</b>'
+            },
+            accessibility: {
+                point: {
+                    valueSuffix: '%'
+                }
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                        // distance: -1
+                    }
+                }
+            },
+            series: [{
+                name: adminsPerCategory.name,
+                data: adminsPerCategory.data
+            }],
+            subtitle: subtitle,
+            credits: credits
+        })
+    }
+});
+
 
 // Administrations per provider
-let adminsPerProvider = {
-    chart: {
-        plotBackgroundColor: null,
-        plotBorderWidth: null,
-        plotShadow: false,
-        type: 'pie'
+$.ajax(baseUrl + 'provider', {
+    area: area,
+    dataType: 'json',
+    cache: false,
+    beforeSend: function () {
+        $('#vax-provider-loading-spinner').show();
     },
-    title: {
-        text: adminsPerProviderTitle
+    data: {
+        area: area
     },
-    tooltip: {
-        pointFormat: '{series.name}: <b>{point.y}</b>'
-    },
-    accessibility: {
-        point: {
-            valueSuffix: '%'
-        }
-    },
-    plotOptions: {
-        pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: {
-                enabled: true,
-                format: '{point.name}: <b>{point.percentage:.1f}%</b>',
-                // distance: -1
-            }
-        }
-    },
-    series: [{
-        name: adminsPerProviderSeriesName,
-        data: adminsPerProviderData
-    }],
-    subtitle: subtitle,
-    credits: credits
-}
-Highcharts.chart('chart-pie-providers', adminsPerProvider);
+    success: function (adminsPerProvider) {
+        $('#vax-provider-loading-spinner').hide();
+        $('#chart-pie-providers').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: adminsPerProvider.title + ' | ' + area
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.y}</b>'
+            },
+            accessibility: {
+                point: {
+                    valueSuffix: '%'
+                }
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '{point.name}: <b>{point.percentage:.1f}%</b>',
+                        // distance: -1
+                    }
+                }
+            },
+            series: [{
+                name: adminsPerProvider.name,
+                data: adminsPerProvider.data
+            }],
+            subtitle: subtitle,
+            credits: credits
+        })
+    }
+});
