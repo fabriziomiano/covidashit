ca0b763a4e8630ed11f471e19e5af1dd01d0e82b
Fabrizio Miano <fabriziomiano@gmail.com>
2022-01-07 13:30:37 +0100

    update FE version

--- settings/__init__.py
+++ settings/__init__.py
@@ -6,7 +6,7 @@ from collections import OrderedDict
 
 from flask_babel import gettext
 
-VERSION = "5.10"
+VERSION = "5.12"
 PAGE_BASE_TITLE = gettext("COVID-19 Italy")
 LOCKDOWN_DAY = dt.datetime(2020, 3, 22)
 PHASE2_DAY = dt.datetime(2020, 5, 4)
